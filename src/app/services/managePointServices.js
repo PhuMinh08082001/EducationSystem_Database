31d7a9f4e112a2f8e6e6a41dacd448ac79672b9c
BuiXuanThong <akirakuma5@gmail.com>
2021-12-05 17:12:09 +0700

    fix register

--- src/app/services/managePointServices.js
+++ src/app/services/managePointServices.js
@@ -4,7 +4,7 @@ let getSubject = (id) => {
     return new Promise((resolve, reject) => {
         try {
             connection.query(
-                'select mana.subjectID, sub.subjectName from managepoint as mana join subject as sub on sub.subjectID = mana.subjectID where mana.studentId = ? and mana.semester = ?', [id, 212],
+                'SELECT * FROM managepoint, subject WHERE managepoint.subjectID = subject.subjectID AND managepoint.studentID = ? AND managepoint.semester = ?', [id, 211],
                 function(err, rows) {
                     console.log(this.sql)
                     if (err) {
@@ -18,8 +18,42 @@ let getSubject = (id) => {
         }
     });
 }
-
+let insertNewRegister = (subjectID, studentID, semester) => {
+    return new Promise((resolve, reject) => {
+        try {
+            connection.query(
+                'INSERT INTO register (subjectID, studentID, semester) VALUES (?, ?, ?)', [subjectID, studentID, semester],
+                function(err, rows) {
+                    if (err) {
+                        reject(err)
+                    }
+                    resolve(rows);
+                }
+            );
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+let checkExist = (subjectID, studentID, semester) => {
+    return new Promise((resolve, reject) => {
+        try {
+            connection.query(
+                'SELECT * FROM register WHERE(subjectID = ? AND studentID = ? AND semester = ?)', [subjectID, studentID, semester],
+                function(err, rows) {
+                    if (err) {
+                        reject(err)
+                    }
+                    resolve(rows);
+                }
+            );
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
 module.exports = {
     getSubject: getSubject,
-
+    insertNewRegister: insertNewRegister,
+    checkExist: checkExist
 };
--- src/routes/student.js
+++ src/routes/student.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var managePointServices = require('../app/services/managePointServices')
+const subjectServices = require('../app/services/subjectServices');
+
 router.get('/viewSubject', function(req, res, next) {
     let id = req.session.passport.user.id;
     managePointServices.getSubject(id).then(async(rows) => {
@@ -14,8 +16,46 @@ router.get('/viewSubject', function(req, res, next) {
 });
 
 router.get('/regisSub', (req, res) => {
-
+    let id = req.session.passport.user.id;
+    subjectServices.getAllSubject().then(async(rows) => {
+        if (req.query.semester) {
+            semester = req.query.semester;
+        } else {
+            semester = rows[0].semester;
+        }
+        for (let i = 0; i < rows.length; i++) {
+            managePointServices.checkExist(rows[i].subjectID, id, rows[i].semester).then(async(rows2) => {
+                console.log(rows2)
+                if(rows2.length > 0) Object.assign(rows[i], {existed: "1"});
+                else Object.assign(rows[i], {existed: "0"});
+            })
+        }
+        subjectServices.getAllSemester(semester).then(async(semesters) => {
+            try {
+                res.render(`student/registerSubject`, {
+                    user: req.user,
+                    rows: rows,
+                    semesters: semesters,
+                    semeste: semester,
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        });
+    });
 })
+router.get('/regisSub/enroll/:subID/:stuID/:semester', function(req, res, next) {
+    let subjectID = req.params.subID;
+    let studentID = req.params.stuID;
+    let semester = req.params.semester;
+    managePointServices.insertNewRegister(subjectID, studentID, semester).then(async(rows) => {
+        try {
+            res.redirect('/student/regisSub/?semester=' + semester)
+        } catch (err) {
+            console.log(err);
+        }
+    })
+});
 
 
 module.exports = router;
