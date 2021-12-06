633c9d9b261b552530a8d3cac0407fcf9029069a
NgoaLong <phu.nguyen0808@hcmut.edu.vn>
2021-12-06 07:12:46 +0700

    fix db

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: 'school1'
+    database: 'school'
 });
 connection.connect(function(error) {
     if (!!error) {
